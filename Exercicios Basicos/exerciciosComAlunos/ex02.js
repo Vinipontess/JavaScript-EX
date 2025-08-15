a339affd1636255ab04dd04885e34c86bc3fef1f
Vinícius Pontes <148925294+Vinipontess@users.noreply.github.com>
2025-08-15 20:46:05 -0300

    Create ex02.js

---
+++ Exercicios Basicos/exerciciosComAlunos/ex02.js
@@ -0,0 +1,9 @@
+let meuAnime = {
+  nome: "Demon Slayer",
+  ano: 2019,
+  produtora: "MAPPA",
+  elenco: ["tanjiro", "zenitsu", "muzan"]
+}
+
+console.log(`O novo filme de ${meuAnime.nome} está prestes a lançar, criado em ${meuAnime.ano} pelo estudio ${meuAnime.produtora}`);
+console.log(`Um dos personagens do anime é o ${meuAnime.elenco[0]}`);
